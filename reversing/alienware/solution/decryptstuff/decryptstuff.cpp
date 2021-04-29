96d3bd8f49be4fe1ab1883cbb45c74b9f08b790b
nicholas-long <nicholas.eden.long@gmail.com>
2021-04-29 17:21:44 -0500

    cleanup and comments

--- reversing/alienware/solution/decryptstuff/decryptstuff.cpp
+++ reversing/alienware/solution/decryptstuff/decryptstuff.cpp
@@ -1,5 +1,5 @@
-// decryptstuff.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// This is the inverse of the crypto functions performed in the embedded xuTaV.dll
+// Which is kind of like pretend ransomware that encrypts everything in the Docs folder of your user (not the actual documents folder thankfully)
 
 #include "pch.h"
 #include <iostream>
@@ -8,11 +8,13 @@
 
 int main()
 {
+	// data passed in from the exe to the DLL
+	// this is the content of the environment variable "OS" after it has been run through some random XORs
 	char passed_in_data[] = "\x2F\x6B\x18\xE4\x9A\x33\xD9\xC7\xA0\x31\x46\x1F\x16\x66\x19\xF7\x00\x00\x04\xFF\x00\x00\x00\x00";
 
 	wchar_t dest[100];
 	ZeroMemory(dest, 100);
-	mbstowcs(dest, passed_in_data, 16);
+	mbstowcs(dest, passed_in_data, 16); // copy in data as multibyte
 
 	wchar_t szProvider[] = L"Microsoft Enhanced RSA and AES Cryptographic Provider";
 
@@ -30,7 +32,7 @@ int main()
 
 	int v5 = lstrlenW(dest);
 
-	if (!CryptHashData(phHash, (const BYTE *)dest, v5, 0))
+	if (!CryptHashData(phHash, (const BYTE *)dest, v5, 0)) // hash data - notice the length (v5) is wrong: it is output of lstrlenW and not accounting for the fact that the chars are wide bytes  
 	{
 		std::cout << std::hex << GetLastError() << std::endl;
 		exit(3);
@@ -39,8 +41,8 @@ int main()
 	if (!CryptDeriveKey(phProv, 0x660Eu, phHash, 0, &phKey))
 		exit(4);
 
-	LPCWSTR lpFileName = L"C:\\Users\\nicho\\Desktop\\ctf\\Confidential2.alien";
-	LPCWSTR lpOutFileName = L"C:\\Users\\nicho\\Desktop\\ctf\\Confidential2.pdf";
+	LPCWSTR lpFileName = L"Confidential2.alien";
+	LPCWSTR lpOutFileName = L"Confidential2.pdf";
 
 	HANDLE v6 = CreateFileW(lpFileName, 0x80000000, 1u, 0i64, 3u, 0x8000000u, 0i64);
 
@@ -80,14 +82,3 @@ int main()
 	CloseHandle(v7);
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
